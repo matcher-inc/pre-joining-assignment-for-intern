ebc3dbebd4af365be3a657a72462d412c5f47fb7
sutekami <115330856+sutekami@users.noreply.github.com>
2026-06-20 15:15:10 +0900

    update: setup 完了後はフラグファイルを置いて実行不可能にする

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+_result=$(find .done.setup -type f 2>&1)
+
+if [[ $? -eq 0 ]]; then
+  echo 'setup was done.' 1>&2
+  exit 1
+fi
+
 # Setup Env
 cp ./backend/.env.example ./backend/.env
 cp ./frontend/.env.example ./frontend/.env
@@ -14,3 +21,5 @@ docker compose run --rm frontend sh -lc 'npm i'
 docker compose run --rm backend sh -lc 'bundle exec rails db:drop db:create db:migrate'
 
 docker compose stop
+
+touch .done.setup

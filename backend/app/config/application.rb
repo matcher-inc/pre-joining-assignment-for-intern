a08b5fabe24eb37228ab9292ffa3c7cb9cacd291
sutekami <115330856+sutekami@users.noreply.github.com>
2026-06-30 22:21:22 +0900

    add: デフォルトの言語設定は :ja とする

--- backend/app/config/application.rb
+++ backend/app/config/application.rb
@@ -32,5 +32,8 @@ module App
     # Configure time zone
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    # I18n
+    config.i18n.default_locale = :ja
   end
 end

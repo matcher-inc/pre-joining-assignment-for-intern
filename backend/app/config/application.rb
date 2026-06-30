63702a9fbbcb5fb55275d52cbf8fa12f86278bb6
sutekami <115330856+sutekami@users.noreply.github.com>
2026-06-30 21:30:45 +0900

    add: rails timezone default asia tokyo

--- backend/app/config/application.rb
+++ backend/app/config/application.rb
@@ -28,5 +28,9 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Configure time zone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
